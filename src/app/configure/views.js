e054763885a4ba9517fa6dba190a07804c268b17
Julien Fournier <fournier.j@sfr.fr>
2017-10-05 07:59:39 +0200

    Wrong merge

--- src/app/configure/views.js
+++ src/app/configure/views.js
@@ -1,6 +1,25 @@
+import randomID from 'random-id'
 module.exports = (app, config) => {
   app.set('view engine', 'pug')
-  app.set('views', [`${process.cwd()}${config.viewsPath}`, __dirname + '/../../../views'])
+  app.set('views', [
+    `${process.cwd()}${config.viewsPath}`,
+    __dirname + '/../../../views',
+  ])
   app.locals.compileDebug = process.env.NODE_ENV !== 'production'
   app.locals.cache = process.env.NODE_ENV === 'production'
+  app.locals.filters = {
+    'async-include': (text, { route, view }) => {
+      const id = randomID(10)
+      return `<div id="${id}"></div>
+        <script type="text/javascript">
+          $(function(){
+            $.post('/lai', {route: '${route}', view: '${view}'})
+              .then(function(data){
+                $('#${id}').html(data)
+              })
+          })
+        </script>
+        `
+    },
+  }
 }
