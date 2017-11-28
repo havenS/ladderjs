93c8c057b1d283d37c75d737fecf606198cb8049
Julien Fournier <fournier.j@sfr.fr>
2017-11-28 10:18:59 +0100

    Defalut expiry for cookie configurable

--- src/app/configure/session.js
+++ src/app/configure/session.js
@@ -3,16 +3,18 @@ import flash from 'connect-flash'
 import cookieParser from 'cookie-parser'
 
 export default app => {
+  const sessionConfig = app.ladderjs.config.session
   app.use(cookieParser())
 
   app.use(
     session({
       resave: true,
       saveUninitialized: false,
+      ...sessionConfig,
       store: new (require('nedb-session-store')(session))({
         filename: 'sessions/sessions.db',
+        defaultExpiry: sessionConfig.defaultExpiry,
       }),
-      ...app.ladderjs.config.session,
     })
   )
 
